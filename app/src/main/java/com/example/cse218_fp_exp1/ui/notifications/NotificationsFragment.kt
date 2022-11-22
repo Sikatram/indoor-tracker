3ba9ed685ca68bed2fcb1f464c61b7566a42195e
ec <earkz0@gmail.com>
2022-11-21 17:25:12 -0800

    Potentially fix performance for more zones

--- app/src/main/java/com/example/cse218_fp_exp1/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/cse218_fp_exp1/ui/notifications/NotificationsFragment.kt
@@ -23,7 +23,6 @@ class NotificationsFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
     private var observer: ProximityObserver? = null
-    private var zone: ProximityZone? = null
     private var handler: ProximityObserver.Handler? = null
     private val cloudCredentials = EstimoteCloudCredentials("beacon-test-eric-chs" , "a0545bb6c94e40729210f58edf665bd2")
     private var telemetryFullScanHandler: ScanHandler? = null
@@ -36,6 +35,13 @@ class NotificationsFragment : Fragment() {
         "d1610eab3fc6f11a0de3a9924280393d" to ("I" to mutableSetOf<Double>(-1.0)), // I
         "257356716b5cd63031e00e52664b2114" to ("N" to mutableSetOf<Double>(-1.0)), // N
         "90fe98ec293340f451d32480c3fe262a" to ("E" to mutableSetOf<Double>(-1.0)), // E
+    )
+
+    private var beacons: Map<String, Pair<Double, Double>> = mapOf(
+        "687572e4da15128f8cc1096f874d1a37" to (0.0 to 3.0), // L
+        "d1610eab3fc6f11a0de3a9924280393d" to (3.0 to 3.0), // I
+        "257356716b5cd63031e00e52664b2114" to (0.0 to 0.0), // N
+        "90fe98ec293340f451d32480c3fe262a" to (2.0 to 0.0), // E
     )
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -56,15 +62,19 @@ class NotificationsFragment : Fragment() {
         refreshButton = binding.refreshLocations
         refreshButton!!.setOnClickListener {
             var text = ""
+            var currentDistances: MutableMap<String, Double?> = mutableMapOf()
             for (entry in distances.entries.iterator()) {
                 var (name, ds) = entry.value
                 if (ds.isEmpty()){
                     text += "Beacon: $name Distance: ??? meters\n"
+                    currentDistances[entry.key] = null
                 } else {
                     text += "Beacon: $name Distance: ${ds.min()} meters\n"
+                    currentDistances[entry.key] = ds.min() -1
                 }
-
             }
+            val (x, y) = calculateCoordinate(currentDistances)
+            text += "Coords: $x, $y"
             textView!!.text = text
         }
         /*
@@ -97,8 +107,8 @@ class NotificationsFragment : Fragment() {
                 var zones: MutableList<ProximityZone> = mutableListOf()
 
                 // Build the zone you want to observer
-                for (i in 0 .. 15) {
-                    var zone = createZone(index = i, step = 1.0, offset = 1.0)
+                for (i in 0 .. 20) {
+                    var zone = createZone(index = i, step = 0.5, offset = 0.5)
                     zones.add(zone)
                 }
 
@@ -150,11 +160,11 @@ class NotificationsFragment : Fragment() {
                         var (name, ds) = entry.value
                         if (inRange.contains(entry.key)) {
                             // within range meters of beacon
-                            ds.add(range)
+                            ds.add(range - step)
                         } else {
                             // outside range meters of beacon
-                            for (i in ds.filter { it <= range }) {
-                                ds.remove(i)
+                            for (i in ds.filter { it <= range-step}) {
+                                ds.remove(i )
                             }
                         }
                         /*
@@ -170,6 +180,87 @@ class NotificationsFragment : Fragment() {
             }
             .build()
         return zone
+    }
+
+    private fun calculateCoordinate(radius: Map<String, Double?>): Pair<Double, Double> {
+        var beaconIDs = radius.keys.toList()
+        // 012, 013, 023, 123
+        var x: Double = 0.0
+        var y: Double = 0.0
+        var p: Pair<Double, Double>? = null
+
+        var m = mapOf<String, Double?>(
+            beaconIDs[0] to radius[beaconIDs[0]],
+            beaconIDs[1] to radius[beaconIDs[1]],
+            beaconIDs[2] to radius[beaconIDs[2]],
+        )
+        p = threePointCalc(m)
+        if (p != null) {
+            x += p.first
+            y += p.second
+        }
+
+        m = mapOf<String, Double?>(
+            beaconIDs[0] to radius[beaconIDs[0]],
+            beaconIDs[1] to radius[beaconIDs[1]],
+            beaconIDs[3] to radius[beaconIDs[3]],
+        )
+        p = threePointCalc(m)
+        if (p != null) {
+            x += p.first
+            y += p.second
+        }
+
+        m = mapOf<String, Double?>(
+            beaconIDs[0] to radius[beaconIDs[0]],
+            beaconIDs[3] to radius[beaconIDs[3]],
+            beaconIDs[2] to radius[beaconIDs[2]],
+        )
+        p = threePointCalc(m)
+        if (p != null) {
+            x += p.first
+            y += p.second
+        }
+
+        m = mapOf<String, Double?>(
+            beaconIDs[3] to radius[beaconIDs[3]],
+            beaconIDs[1] to radius[beaconIDs[1]],
+            beaconIDs[2] to radius[beaconIDs[2]],
+        )
+        p = threePointCalc(m)
+        if (p != null) {
+            x += p.first
+            y += p.second
+        }
+        return x/4 to y/4
+    }
+
+    private fun threePointCalc(beaconRadius: Map<String, Double?>): Pair<Double, Double>? {
+        val idIter = beaconRadius.keys.iterator()
+        var key = idIter.next()
+
+        val (x1, y1) = beacons[key]?: (-1.0 to -1.0)
+        val r1 = beaconRadius[key] ?: return null
+
+        key = idIter.next()
+        val (x2, y2) = beacons[key]?: (-1.0 to -1.0)
+        val r2 = beaconRadius[key]?: return null
+
+
+        key = idIter.next()
+        val (x3, y3) = beacons[key]?: (-1.0 to -1.0)
+        val r3 = beaconRadius[key]?: return null
+
+        val A = 2*x2 - 2*x1
+        val B = 2*y2 - 2*y1
+        val C = r1*r1 - r2*r2 - x1*x1 + x2*x2 - y1*y1 + y2*y2
+        val D = 2*x3 - 2*x2
+        val E = 2*y3 - 2*y2
+        val F = r2*r2 - r3*r3 - x2*x2 + x3*x2 - y2*y2 + y3*y3
+        val x = (C*E - F*B) / (E*A - B*D)
+        val y = (C*D - A*F) / (B*D - A*E)
+        return x to y
+
     }
 
     override fun onDestroyView() {
